7fdd70fe1cd01962ef74a3008c9fdaa2f9616854
OmarMWarraich <earthling986@gmail.com>
2023-04-25 20:45:05 +0500

    test: Pass

--- drkiq/test/jobs/counter_job_test.rb
+++ drkiq/test/jobs/counter_job_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class CounterJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert_equal 42, CounterJob.perform_now
+  end
 end
